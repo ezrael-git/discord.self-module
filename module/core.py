b714368fe3e760c6241db94800f3f72826ebed45
Azrael <91268618+ezrael-git@users.noreply.github.com>
2021-12-09 21:59:21 +0500

    Update core.py

--- module/core.py
+++ module/core.py
@@ -21,8 +21,8 @@ def git(file):
   if os.path.exists("./" + file) == True:
     return
   g = Github()
-  r = g.get_repo(git_info["repo"])
-  c = r.get_contents(file).decoded_content.decode()
+  r = g.get_repo(git_info["author"] + "/" + git_info["repo"])
+  c = r.get_contents("/module/" + file).decoded_content.decode()
   f = open(file, w)
   f.write(str(c))
   f.close()
