330ec8fce23549e9e358d48976a14218f146f032
Azrael <91268618+ezrael-git@users.noreply.github.com>
2021-12-10 19:56:00 +0500

    Update mass_dm.py

--- module/orders/mass_dm.py
+++ module/orders/mass_dm.py
@@ -2,10 +2,12 @@
 mass dms a channel
 """
 
-worker.bot.loop.create_task(worker.bot.wait_until_ready())
 
-target = worker.bot.get_channel(918533562033139804)
+async def cum():
+  await worker.bot.wait_until_ready()
+  target = worker.bot.get_channel(918533562033139804)
+  while True:
+    await target.send("cum")
+    await asyncio.sleep(60)
 
-while True:
-  worker.bot.loop.create_task(asyncio.sleep(60))
-  worker.bot.loop.create_task(target.send("cum"))
+worker.bot.loop.create_task(cum)
