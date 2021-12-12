6aa21a52620cf8c1b22c3268d0e26eaaec9aa21a
Azrael <91268618+ezrael-git@users.noreply.github.com>
2021-12-12 17:27:10 +0500

    Update network.py

--- module/network.py
+++ module/network.py
@@ -6,6 +6,7 @@ class Network:
   def __init__(self, manager: list, workers: list):
     self.manager = manager[0]
     self.workers = workers
+    self.head = self.manager
     team = [self.manager]
     for i in self.workers: team.append(i)
 
@@ -14,4 +15,28 @@ class Network:
     for i in self.team: temp.append(i.bot)
     return temp
 
-  def 
+  def wait_until_ready(self):
+    for member in self.team:
+      await member.bot.wait_until_ready()
+    return True
+
+  def join_guild(self, invite, **kwargs):
+    wait = kwargs.get("wait", 60)
+    for member in team:
+      try:
+        await member.bot.join_guild(invite)
+        await asyncio.sleep(wait)
+      except:
+        continue
+    return True
+
+  def leave_guild(self, id, **kwargs):
+    wait = kwargs.get("wait", 60)
+    for member in team:
+      try:
+        await member.bot.leave_guild(id)
+        await asyncio.sleep(wait)
+      except:
+        continue
+    return True
+    
